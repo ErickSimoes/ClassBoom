e7b2f5866d4af7cc8480f71db03da49250db1de6
henrey22 <pedro.novaes@nave.org.br>
2021-10-19 22:41:09 -0300

    Instantiate bombs done

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -6,19 +6,31 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     internal Rigidbody2D rb;
+
+    [Header("Movement")]
     [Range(100,300)]
     [SerializeField] float speed = 100;
     Vector2 direction;
 
+    //buttons
+    [SerializeField] string lauchButtonName = "Launch";
+
+    //Childreens
+    internal BombController _bombController;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _bombController = FindObjectOfType<BombController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         rb.velocity = direction.normalized * speed * Time.fixedDeltaTime;
+
+        //inputs
+        if (Input.GetButtonDown(lauchButtonName))
+            _bombController.LauchBomb();
     }
 }
