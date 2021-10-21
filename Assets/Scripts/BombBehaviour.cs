fd8e4cc77f8159c46cb4f12abf1c3c7017451535
henrey22 <pedro.novaes@nave.org.br>
2021-10-21 18:40:08 -0300

    bomb is exploding

--- Assets/Scripts/BombBehaviour.cs
+++ Assets/Scripts/BombBehaviour.cs
@@ -5,13 +5,46 @@ using UnityEngine;
 public class BombBehaviour : MonoBehaviour
 {
     [SerializeField] float bombCoolddown = 0.5f;
-    // Start is called before the first frame update
+    [SerializeField] LayerMask Destructable;
+
+    [SerializeField] Collider2D[] colls;
+    [SerializeField] SpriteRenderer spr;
+    [SerializeField] SpriteRenderer[] chilldreenSpr;
+
     void Start()
     {
+        foreach(Collider2D coll in colls) {
+            coll.enabled = false;
+        }
+
+        foreach (SpriteRenderer sprChild in chilldreenSpr) {
+            sprChild.enabled = false;
+        }
+
         Invoke("explode", bombCoolddown);
+        Invoke("endOfExplosion", bombCoolddown + 0.5f);
     }
 
+
+
     void explode() {
+        spr.enabled = false;
+
+        foreach(Collider2D coll in colls) {
+            coll.enabled = true;
+        }   
+
+        foreach (SpriteRenderer sprChild in chilldreenSpr) {
+            sprChild.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(exploded == true && other.gameObject.tag == "Player")
+        other.gameObject.GetComponent<PlayerCollision>().Killed();
+    }
+    void endOfExplosion(){
         Destroy(this.gameObject);
     }
+
 }
