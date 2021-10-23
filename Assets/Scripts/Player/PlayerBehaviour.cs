4cd05e9e54ab88b8554e4985da0b59f7866620ca
henrey22 <pedro.novaes@nave.org.br>
2021-10-23 17:49:08 -0300

    player animation added

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -11,6 +11,10 @@ public class PlayerBehaviour : MonoBehaviour
     [Range(100,300)]
     [SerializeField] float speed = 100;
     Vector2 direction;
+    Vector2 movingInputs;
+
+    [SerializeField] Animator anim;
+    [SerializeField] SpriteRenderer graphic;
 
     //buttons
     [SerializeField] string lauchButtonName = "Launch";
@@ -26,7 +30,23 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Update()
     {
-        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movingInputs.x = Input.GetAxisRaw("Horizontal");
+        movingInputs.y = Input.GetAxisRaw("Vertical");
+
+        if (movingInputs.x > 0) {
+            graphic.flipX = false;
+        } else if(movingInputs.x < 1) {
+            graphic.flipX = true;
+        }
+
+        print("velocidade x : " + movingInputs.x + "  velocidade y : " + movingInputs.y + "  movement : " + movingInputs.SqrMagnitude());
+
+        //animation
+        anim.SetFloat("xVelocity", Mathf.Abs(movingInputs.x));
+        anim.SetFloat("yVelocity", movingInputs.y);
+        anim.SetFloat("speed", movingInputs.SqrMagnitude());
+
+        direction = movingInputs;
         rb.velocity = direction.normalized * speed * Time.fixedDeltaTime;
 
         //inputs
