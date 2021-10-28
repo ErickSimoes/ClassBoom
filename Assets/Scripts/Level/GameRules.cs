3a980f0596a2ebf6bed1cb4f9d6b66cf25de0fda
henrey22 <pedro.novaes@nave.org.br>
2021-10-28 14:48:11 -0300

    now windoor always is spawning inside a box

--- Assets/Scripts/Level/GameRules.cs
+++ Assets/Scripts/Level/GameRules.cs
@@ -2,37 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-public class GameRules : MonoBehaviour
-{
+public class GameRules : MonoBehaviour {
     int totalOfEnemies;
     [SerializeField] private string enemyTag;
     [SerializeField] GameObject winDoor;
     [SerializeField] Tilemap tilemap;
+    [SerializeField] Tile destructibleTile;
     [SerializeField] Vector2 limitX, limitY;
     private Vector3 randomPos;
     private int winDoorInstantiate = 0;
 
-    void Update() {
+    private void Update() {
         totalOfEnemies = GameObject.FindGameObjectsWithTag(enemyTag).Length;
         if(totalOfEnemies <= 0)
             CreateDoor();
     }
 
-    void CreateDoor() {
+    private void CreateDoor() {
         if(winDoorInstantiate < 1) {
             randomPos.x = Random.Range(limitX.x, limitX.y);
             randomPos.y = Random.Range(limitY.x, limitY.y);
             Vector3Int cell = tilemap.WorldToCell(randomPos);
             Tile tile = tilemap.GetTile<Tile>(cell);
 
-            if (tile == destructibleTile) {
-			    Vector3 CenterOfCell = tilemap.GetCellCenterWorld(cell);
-                Instantiate(winDoor, CenterOfCell, Quaternion.identity); 
-                winDoorInstantiate++;
-		    }
-            // Vector3 CenterOfCell = tilemap.GetCellCenterWorld(cell);
-            // Instantiate(winDoor, CenterOfCell, Quaternion.identity); 
-            // winDoorInstantiate++;
+            while (tile != destructibleTile) {
+                randomPos.x = Random.Range(limitX.x, limitX.y);
+                randomPos.y = Random.Range(limitY.x, limitY.y);
+                cell = tilemap.WorldToCell(randomPos);
+                tile = tilemap.GetTile<Tile>(cell);
+
+                if(tile == destructibleTile) {
+                    break;
+                }
+            }
+
+			Vector3 CenterOfCell = tilemap.GetCellCenterWorld(cell);
+            Instantiate(winDoor, CenterOfCell, Quaternion.identity); 
+            winDoorInstantiate++;
         }
     }
 }
